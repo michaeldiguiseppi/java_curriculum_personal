8c2ff41065dcabb059807185fbb2a15dfd4822a5
Dize Hacioglu <dh1272@nyu.edu>
2015-11-17 11:09:36 -0700

    added singer test

---
+++ src/test/java/challenges/SingerTest.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SingerTest {
+
+    // @Test
+    // public void sing_ShouldSing() {
+	   //  Singer singer = new Singer();
+	   //  assertEquals("La la la", singer.sing());
+    // }
+
+}
