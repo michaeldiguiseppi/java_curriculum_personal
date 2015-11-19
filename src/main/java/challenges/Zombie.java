5a8f94bd92528bb9a88bc1401a06a0b375ddffca
Dize Hacioglu <dh1272@nyu.edu>
2015-11-19 15:38:54 -0700

    added constructors for zombie class

--- src/main/java/challenges/Zombie.java
+++ src/main/java/challenges/Zombie.java
@@ -9,6 +9,13 @@ public class Zombie implements Person{
 	String[] victimPleas;
 	HashMap<String, Human[]> stomachContents = new HashMap<String, Human[]>();
 
+	// Constructors
+	public Zombie(String name){
+	}
+
+	public Zombie(String name, int health){
+	}
+
   // Override isDead() function from Person class
 	@Override
 	public boolean isDead(){
