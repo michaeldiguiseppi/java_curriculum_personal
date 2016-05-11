b8788fabbb64dd66e3edfc7790c700a492137a79
BenAychh <ben.tomas.hernandez@gmail.com>
2016-05-11 09:52:11 -0600

    Refactored Zombie and Human

--- Unit-5-junit/src/main/java/Human.java
+++ Unit-5-junit/src/main/java/Human.java
@@ -1,17 +1,78 @@
 public class Human implements Person {
-
-	String name, plea;
+    /**
+     * The name of the zombie.
+     */
+	private String name;
+    /**
+     * What the human says when the zombie eats them.
+     */
+    private String plea;
+    /**
+     * The age of the human.
+     */
 	int age;
+    /**
+     * Whether or not the person has been eaten.
+     */
 	private boolean eaten;
 
-  public Human(String name, int age){
-  }
+    /**
+     * Constructor, initiates with name and age.
+     * @param name the name of the human.
+     * @param age how old the human is.
+     */
+    public Human(String name, int age){
 
-  public Human(String name, int age, String plea){
-  }
+    }
 
-  @Override
-  public boolean isDead(){
+    /**
+     * Constructor, initiates with name, age and plea.
+     * @param name the name of the human.
+     * @param age the age of the human.
+     * @param plea what the human says when the zombie eats them.
+     */
+    public Human(String name, int age, String plea){
+
+    }
+
+    /**
+     * Returns if the human is dead.
+     * @return true if the human is dead.
+     */
+    @Override
+    public boolean isDead(){
+
 	}
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlea() {
+        return plea;
+    }
+
+    public void setPlea(String plea) {
+        this.plea = plea;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isEaten() {
+        return eaten;
+    }
+
+    public void setEaten(boolean eaten) {
+        this.eaten = eaten;
+    }
 }
--- Unit-5-junit/src/main/java/Zombie.java
+++ Unit-5-junit/src/main/java/Zombie.java
@@ -1,17 +1,48 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Zombie implements Person{
 
-	String name;
-	int health;
-	Human[] knownHumans;
-	String[] victimPleas;
-	HashMap<String, Human[]> stomachContents = new HashMap<String, Human[]>();
+	/**
+	 * The name of the zombie.
+	 */
+	private String name;
+	/**
+	 * The amount of health the zombie has.
+	 */
+	private int health;
+	/**
+	 * The humans he has come across.
+	 */
+	private HashMap<String, ArrayList<Human>> humans;
+	/**
+	 * The pleas of the victims.
+	 */
+	private ArrayList<String> victimPleas;
+	/**
+	 * What is this even?
+	 */
+	private ArrayList<Human> stomachContents = new ArrayList<>();
 
+	/**
+	 * Constructor, initializes the zombie with a name as well as our ArrayLists and HashMaps
+	 * @param name
+     */
 	public Zombie(String name){
+        // Set name variable here.
+        humans = new HashMap<>();
+        humans.put("known", new ArrayList<>());
+        humans.put("eaten", new ArrayList<>());
 	}
 
-	public Zombie(String name, int health){
+	/**
+	 * Constructor, initialize the zombie with a name and initial health.
+	 * @param name
+	 * @param health
+     */
+	public Zombie(String name, int health) {
+        this(name);
+        // Set health variable here.
 	}
 
 	@Override
